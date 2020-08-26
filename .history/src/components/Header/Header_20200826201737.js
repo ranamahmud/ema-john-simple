a259d2d67f559c984cf8c8ffcd57137c5495c5fc
Md Rana Mahmud <ranamd0@gmail.com>
2020-08-26 20:20:55 +0600

    logo added

---
+++ .history/src/components/Header/Header_20200826201737.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../images/log.png'
+const Header = () => {
+    return (
+        <div>
+            <h1>This is header</h1>
+        </div>
+    );
+};
+
+export default Header;
---
+++ .history/src/components/Header/Header_20200826201903.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../images/logo.png'
+const Header = () => {
+    return (
+        <div className="Header">
+            <img src={logo} alt=""/>
+        </div>
+    );
+};
+
+export default Header;
